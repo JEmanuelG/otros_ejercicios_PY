c636559c0bcaab3318a4a31e902636ed0120afd7
Emanuel Guaraz <guarazjemanuel@gmail.com>
2022-09-06 20:39:43 -0300

    Commit inicial

---
+++ ejercicio1_sec13.py
@@ -0,0 +1,7 @@
+'''
+Crear una funcion "buscar" que mediante una expresion recgular indique si una palabra estpa en una frase
+Probar la función "buscar" con estas frases:
+    texto = "Esto es una frase de pruebas para hacer busquedas"
+    palabra_a_buscar = "frase"
+En caso de encontrar la palabra en la frase, indicar en que posición empieza y en cual finaliza.
+'''
